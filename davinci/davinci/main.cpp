8810dbaa1e963cead21596f211ff542b426ff7b8
IOTnewb <42164398+IOTnewb@users.noreply.github.com>
2018-08-16 11:30:41 +0900

    첫 에러

--- davinci/davinci/main.cpp
+++ davinci/davinci/main.cpp
@@ -1,11 +1,84 @@
 #include <iostream>
 #include <stdlib.h>
+#include <time.h>
 
+using namespace std;
 
 int main()
 {
-	// 1. 24장의 카드중 무작위 4장을 플레이어들이 받는다.
+	srand((int)time(NULL));
 
+	// 1. 24장의 카드중 무작위 4장을 플레이어들이 받는다.
+	int card[24] = {10 , 11 , 20 , 21 , 30 , 31 , 40 , 41 , 50 , 51 , 60 , 61 , 70 , 71 , 80 , 81 , 90 , 91 , 100 , 101 , 110 , 111 , 120 , 121};
+
+	int p1[12];
+	int p2[12];
+
+	int a;
+
+	for (int i = 0; i < 4; i++)
+		{
+		a = (rand() % 25);
+
+		p1[i] = card[a];
+
+		while (1)
+		{
+			if (p1[i] == 0)
+			{
+
+			a = (rand() % 25);
+
+			p1[i] = card[a];
+
+			}
+			else	break;
+		}
+
+		card[a] = 0;
+
+		}
+
+	for (int i = 0; i < 4; i++)
+	{
+
+		cout << p1[i] << endl;
+
+	}
+
+
+	for (int i = 0; i < 4; i++)
+	{
+
+		a = (rand() % 25);
+
+		p2[i] = card[a];
+
+		while (1)
+		{
+
+			if (p2[i] == 0)
+			{
+
+				a = (rand() % 25);
+				p2[i] = card[a];
+
+			}
+
+			else if (p2[i] != 0)
+			break;
+		}
+
+		card[a] = 0;
+
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+
+		cout << p2[i] << endl;
+
+	}
 
 
 	// 2. 플레이어들은 받은 4장의 카드를 룰에 맞게 정렬한다.
