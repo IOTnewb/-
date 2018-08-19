049f323bcc9032b2095a46b770b85300fb1d7940
kimkijoo <tmzkdl5783@naver.com>
2018-08-19 14:03:32 +0900

    틀린거 수정(1)

--- davinci/davinci/main.cpp
+++ davinci/davinci/main.cpp
@@ -152,7 +152,24 @@ int main()
 		cout << "추측할 위치와 카드숫자" << endl;
 
 		cin >> locate;
+		while (1)
+		{
+			if (locate > 3) {
+				cout << "위치을 다시  입력하시오" << endl;
+				cin >> locate;
+			}
+			if (locate < 4) break;
+		}
 		cin >> value;
+		while (1)
+		{
+			if (value != 10 && value != 11 && value != 20 && value != 21 && value != 30 && value != 31 && value != 40 && value != 41 && value != 50 && value != 51 && value != 60 && value != 61 && value != 70 && value != 71 && value != 80 && value != 81 && value != 90 && value != 91 && value != 100 && value != 101 && value != 110 && value != 111 && value != 120 && value != 121) {
+				cout << "값을 다시  입력하시오" << endl;
+				cin >> value;
+			}
+			if (value == 10 || value == 11 || value == 20 || value == 21 || value == 30 || value == 31 || value == 40 || value == 41 || value == 50 || value == 51 || value == 60 || value == 61 || value == 70 || value == 71 || value == 80 || value == 81 || value == 90 || value == 91 || value == 100 || value == 101 || value == 110 || value == 111 || value == 120 || value == 121)
+				break;
+		}
 
 		if (computer[locate] == value)	result[locate] = value;
 		else score = score - 10;
